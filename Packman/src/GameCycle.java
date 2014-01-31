b15e2098321b06245a61ab4638f2e8d8697bf8fa
Seriy112 <qsdc@mail.ru>
2014-01-31 19:58:17 +0200

    .

--- Packman/src/GameCycle.java
+++ Packman/src/GameCycle.java
@@ -1,10 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class GameCycle {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+
+public class GameCycle extends JFrame {
+	private JPanel panel;
 	
 	public static void main(String[] args) {
 		
 
 	}
+	
+	private GameCycle ()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 500);
+		panel = new JPanel() {
+			public void paint(Graphics g) {
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 0, 640, 500);
+				g.setColor(Color.BLUE);
+
+			
+			}
+		};
+		panel.setSize(500, 500);
+		add(panel);
+
+	}
+	
+	
 
 }
