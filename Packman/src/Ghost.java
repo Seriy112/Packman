3afd735a6c0f9c4c05edebdb45d8ba703c6862fa
Seriy112 <qsdc@mail.ru>
2014-02-11 20:51:55 +0200

    Create

--- Packman/src/Ghost.java
+++ Packman/src/Ghost.java
@@ -1,4 +1,31 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
 
 public class Ghost {
+	private int xPosition;
+	private int yPosition;
+	private int width=15;
+	private int height=15;
 
+	
+
+	
+
+	public Ghost(int xPosition, int yPosition){
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+
+	}	
+	
+	public void pic(Graphics g){
+		g.setColor(Color.RED);
+		g.fillOval(xPosition, yPosition,width, height);
+
+		
+		
+		   
+	}
+	
 }
